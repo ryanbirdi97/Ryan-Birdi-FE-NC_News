4b9955d80c266e453363700e3159054e6fe1721c
ryanbirdi97 <ryanbirdi97@icloud.com>
2022-07-25 15:18:51 +0100

    shows list of articles from api

---
+++ nc-news/src/components/ArticleList.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import * as api from "../utils/api";
+import ArticleCard from "./ArticleCard";
+
+export default function ArticleList() {
+  const [articleArr, setArticleArr] = useState([]);
+
+  useEffect(() => {
+    api.fetchArticles().then((users) => {
+      console.log(users);
+      setArticleArr(users);
+    });
+  }, []);
+
+  return (
+    <ul>
+      {articleArr.map((article) => {
+        return <ArticleCard article={article} />;
+      })}
+    </ul>
+  );
+}
---
+++ nc-news/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>NC News</h1>
+    </div>
+  );
+}
